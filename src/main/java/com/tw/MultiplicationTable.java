5c470c120cc82da19fad919bbf1ea0a800ac7429
kunwei <kwei@thoughtworks.com>
2025-08-26 13:39:40 +0800

    first commit

---
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        System.out.println(multiplicationTable.create(1, 9));
+    }
+
+    public String create(int start, int end) {
+        return null;
+    }
+}
